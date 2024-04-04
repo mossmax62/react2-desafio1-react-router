85eb679280fa60fc47677569783ab53213468bc8
Maximiliano Mussuto <mossmax62@gmail.com>
2024-04-04 20:31:02 -0300

    Estilo not found

--- src/layout/NotFound.jsx
+++ src/layout/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>Opps parece que hemos perdido el rumbo</div>
+    <>
+      <div className='container'>
+        <div>Opps parece que hemos perdido el rumbo</div>
+        <Link to='/' className='btn btn-primary'>Home</Link>
+      </div>
+    </>
   )
 }
 
